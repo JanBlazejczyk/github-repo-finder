cfcce39cb3d94581df8ed9d1f16dfe437ec9b921
JanBlazejczyk <JB.Blazejczyk@gmail.com>
2022-01-06 17:10:53 +0100

    added key prop to repos listing

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -1,13 +1,13 @@
 export default function Repo({ repository }) {
 
     if (repository !== undefined) {
-        const { name, stargazers_count, description, url } = repository;
+        const { name, stargazers_count, description, html_url, id } = repository;
         return (
-            <div className="repo">
+            <div className="repo" key={id}>
                 <p>{name}</p>
                 <p>{`Stars: ${stargazers_count}`}</p>
                 <p>{description}</p>
-                <p><a href={url}>See on GitHub</a></p>
+                <p><a href={html_url} target="_blank" rel="noopener noreferrer">See on GitHub</a></p>
             </div>
         );
     } else {
