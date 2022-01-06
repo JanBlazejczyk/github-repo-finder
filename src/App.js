2ce4b40a3748f9351851929001aba291619641d5
JanBlazejczyk <JB.Blazejczyk@gmail.com>
2022-01-06 14:50:24 +0100

    added a method for sorting the repos

--- src/App.js
+++ src/App.js
@@ -7,18 +7,24 @@ function App() {
   const [searchQuery, setSearchQuery] = useState("");
   const [repos, setRepos] = useState([]);
 
+  const sortArrayOfReposObjectsByStars = (arr) => {
+    return arr.sort((a, b) =>
+      // if repos have the same number of stars, sort them by name
+      (a.stargazers_count > b.stargazers_count) ? 1 : (a.stargazers_count === b.stargazers_count) ? ((a.name > b.name) ? 1 : -1) : -1);
+  }
+
   const handleSearchQueryChange = (event) => {
     setSearchQuery(event.target.value);
   }
   const handleSearch = (event) => {
     event.preventDefault();
-    // getUserRepos(searchQuery);
     saveRepos();
   }
 
   const saveRepos = () => {
     getUserRepos(searchQuery)
-      .then(data => setRepos(data))
+      .then(data => sortArrayOfReposObjectsByStars(data))
+      .then(sortedRepos => setRepos(sortedRepos))
       .catch(error => console.error(error));
   }
 
