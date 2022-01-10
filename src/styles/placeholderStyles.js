f29dd8663cb89797fe0128191d4c2695e629c444
JanBlazejczyk <JB.Blazejczyk@gmail.com>
2022-01-10 17:40:29 +0100

    added general object to placeholders styling

--- src/styles/placeholderStyles.js
+++ src/styles/placeholderStyles.js
@@ -1,18 +1,31 @@
+// general styles (the same for both types of placeholders) are to be changed in the general object
+// in order to avoid the necessity to change them in more than one place
+const general = {
+  textAlign: "center",
+  margin: "20px",
+  borderRadius: "0.4rem",
+}
+
 const placeholderStyles = {
-    username: {
-        textAlign: "center",
-        margin: "20px",
-        borderRadius: "0.4rem",
-        width: "15vw",
-        height: "30px",
-    },
-    repo: {
-        textAlign: "center",
-        margin: "20px",
-        borderRadius: "0.4rem",
-        width: "93vw",
-        height: "80px",
-    }
+  username: {
+    // general styles
+    textAlign: general.textAlign,
+    margin: general.margin,
+    borderRadius: general.borderRadius,
+    // specific styles
+    width: "15vw",
+    height: "30px",
+  },
+
+  repo: {
+    // general styles
+    textAlign: general.textAlign,
+    margin: general.margin,
+    borderRadius: general.borderRadius,
+    // specific styles
+    width: "93vw",
+    height: "80px",
+  }
 }
 
 export default placeholderStyles;
